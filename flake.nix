acba865a0c8e21837d98ecdb922cdf75213da9ee
Diogo Correia <me@diogotc.com>
2023-12-17 20:23:18 +0100

    Update to Velocity 3.3.0-SNAPSHOT

--- flake.nix
+++ flake.nix
@@ -8,6 +8,6 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
-        devShell = pkgs.mkShell { buildInputs = with pkgs; [ gradle jdk11 ]; };
+        devShell = pkgs.mkShell { buildInputs = with pkgs; [ gradle jdk17 ]; };
       });
 }
